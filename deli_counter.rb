bda0551c6c1ce32402ecb2a615981601f1e2efa0
Kevin Boston <kbos22@gmail.com>
2020-05-05 15:33:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,12 @@
 katz_deli = []
 
 def line(katz_deli)
-  current_line = ":"
+  current_line = ": "
   the_line = "The line is currently"
   if katz_deli.length == 0
     puts the_line + " empty."
   else katz_deli.each_with_index do |name, index|
-      current_line << "#{index+1}. #{name} "
+      current_line << " #{index+1}. #{name}"
     end
     puts the_line + current_line
     end
