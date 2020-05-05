468625758a358cd506f06dee55f9c8cb4df14daf
Kevin Boston <kbos22@gmail.com>
2020-05-05 14:34:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,9 +20,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  puts katz_deli[0]
+  puts "Currently serving #{katz_deli[0]}"
   katz_deli.shift
   if katz_deli == []
     puts "There is nobody waiting to be served!"
   end
+end
 end
